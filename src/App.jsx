9f844cd48d80f137bdff5cfc8c81fa53a542e048
why_code <119089018+VWiman@users.noreply.github.com>
2024-08-30 11:17:45 +0200

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -70,15 +70,61 @@ function App() {
 
 	return (
 		<>
-			{recipeList.length > 0 ? (
-				recipeList.map((recipe) => (
-					<div key={recipe.idMeal}>
-						{/* Each meal goes here, extract from components to perform CRUD, example would <TitleImage props={recipe.strMeal recipe.strMealThumb} > */}
+			<div className="left">
+				<h3 className="recipe-header">recipes</h3>
+				<ul id="recipes"></ul>
+				{recipeList.length > 0 ? (
+					recipeList.map((recipe) => (
+						<div key={recipe.idMeal}>
+							<p>{recipe.strMeal}</p>
+							{/* Each meal goes here, extract from components to perform CRUD, example would <TitleImage props={recipe.strMeal recipe.strMealThumb} > */}
+						</div>
+					))
+				) : (
+					<p>No recipes found.</p>
+				)}
+			</div>
+
+			<div className="right">
+				<div className="inner-container-row">
+					<div className="inner-left">
+						<div className="rc-header">
+							<h1 id="display-title">Title</h1>
+						</div>
+						{/* <!-- Stars and rating code start ozay --> */}
+						<div id="rating-container"></div>
+						{/* <!-- Stars and rating code end ozay --> */}
+						<div class="rc-info">
+							<h3>Ingredients</h3>
+							<p id="display-ingredients"></p>
+						</div>
+						<div>
+							<h3>Instructions</h3>
+							<p id="display-instructions">instructions</p>
+						</div>
 					</div>
-				))
-			) : (
-				<p>No recipes found.</p>
-			)}
+					<div className="rc-img">
+						<img />
+					</div>
+				</div>
+			</div>
+			<div className="addnew" id="addnew">
+				<h2>Add new recipe</h2>
+				<form id="form">
+					<label for="input-title">Title</label>
+					<input type="text" id="input-title" placeholder="Title of the recipie" />
+					<label for="input-ingredients">Ingredients</label>
+					<input type="text" id="input-ingredients" placeholder="Comma seperated ingredients" />
+					<label for="input-instructions">Instructions:</label>
+					<input type="text" id="input-instructions" placeholder="Enter instructions" />
+					<label for="input-image">Image name:</label>
+					<input type="text" id="input-image" placeholder="image-5, image-6" />
+					<button type="submit" id="new-recipe-button">
+						Add now!
+					</button>
+					<small id="input-warning"></small>
+				</form>
+			</div>
 		</>
 	);
 }
